13531482120916840e46378ba94ae90ab125ce76
NguyenHanh-codegym <79798747+NguyenHanh-codegym@users.noreply.github.com>
2021-05-26 17:49:23 +0700

    Add files via upload
    công ty

---
+++ cách sử dụng git/Ôn_tập/xuly1.js
@@ -0,0 +1,11 @@
+addHtmlTableRow()
+function addHtmlTableRow() {
+    var table = document.getElementById('table');
+    newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell2 = newRow.insertCell(1);
+    cell3 = newRow.insertCell(2);
+
+    
+
+};
