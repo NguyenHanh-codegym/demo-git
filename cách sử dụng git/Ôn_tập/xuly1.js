3e49f5134c9fc81adfd24ddbfcc2e70a3fc3c7f0
nguyehanh <thanhhanh2685@examplegmail.com>
2021-05-26 20:50:08 +0700

    ôn tập

--- cách sử dụng git/Ôn_tập/xuly1.js
+++ cách sử dụng git/Ôn_tập/xuly1.js
@@ -1,10 +1,27 @@
 addHtmlTableRow()
 function addHtmlTableRow() {
-    var table = document.getElementById('table');
-    newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell2 = newRow.insertCell(1);
-    cell3 = newRow.insertCell(2);
+    var table = document.getElementById('list'),
+    newRow = table.insertRow(table.length),
+    cell1 = newRow.insertCell(0),
+    cell2 = newRow.insertCell(1),
+    cell3 = newRow.insertCell(2),
+    cell4 = newRow.insertCell(3),       
+    cell5 = newRow.insertCell(4),       
+    cell6 = newRow.insertCell(5),       
+
+    studCode = document.getElementById("student_code").value,
+    surName = document.getElementById("surname").value,
+    Email = document.getElementById("email").value,
+    telePhone = document.getElementById("phone_number").value;
+    btnEdit = document.getElementById("btn_edit").value;
+    btnDelete = document.getElementById("btn_delete").value;
+ console.log(surName);
+    cell1.innerHtml = studCode;
+    cell2.innerHtml = surName;
+    cell3.innerHtml = Email;
+    cell4.innerHtml = telePhone;
+    cell5.innerHtml = btnEdit;
+    cell6.innerHtml = btnDelete;
 
     
 
