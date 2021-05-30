de3436f60cc34ee197ebda292d47c19c8f272212
nguyehanh <thanhhanh2685@examplegmail.com>
2021-05-30 13:41:16 +0700

    ôn tập

--- cách sử dụng git/Ôn_tập/xuly.js
+++ cách sử dụng git/Ôn_tập/xuly.js
@@ -1,4 +1,5 @@
 let list = [];
+let updateIndex;
 displaytbodyList();
 function displaytbodyList() {
 
@@ -9,8 +10,8 @@ function displaytbodyList() {
         table_html += `<td>${list[i][1]} </td>`;
         table_html += `<td>${list[i][2]} </td>`;
         table_html += `<td>${list[i][3]} </td>`;
-        table_html += `<td> Edit </td>`;;
-        table_html += `<td> Delete </td>`;;
+        table_html += `<td> <a href="javascript:;" onclick="Edit(${i})">Edit</a> </td>`;;
+        table_html += `<td> <a href="javascript:;" onclick="Delete(${i})">Delete</a> </td>`;;
         table_html += '</tr >';
 
     }
@@ -82,22 +83,17 @@ function validate() {
     }
     return status;
 }
-function SelectedRowtoInputEdit() {
+function Edit(index) {
     let student_code            = list[index ][0];
     let surname                 = list[index ][1];
-    let phone_number            = list[index ][2];
-    let email                   = list[index ][3];
-   console.log(student_code);
+    let email                   = list[index ][2];
+    let phone_number            = list[index ][3];
+   
     document.getElementById('student_code').value           = student_code;
-    document.getElementById('surname ').value               = surname ;
+    document.getElementById('surname').value                = surname ;
+    document.getElementById('email').value                 = email ;
     document.getElementById('phone_number').value           = phone_number;
-    document.getElementById('email ').value                 = email ;
-
-
-    //cho nút cập nhật hiện lên, và ẩn nút đăng ký đi
-    /*
-        nut_cap_nhat, nut_dang_ky
-    */
+    
     let btn_update = document.getElementById('btn_update');
     let btn_register  = document.getElementById('btn_register');
     btn_update.style.display  = 'inline';
@@ -109,65 +105,37 @@ function SelectedRowtoInputEdit() {
 
 }
 
-// //cập nhật dữ liệu
-// function cap_nhat_sinh_vien( index , ma_sv, ten_sv, sdt ){
-//     //kiểm tra các giá trị truyền vào đúng chưa
-//     console.log( `chi_so: ${index }` );
-//     console.log( `ma_sv: ${ma_sv}` );
-//     console.log( `ten_sv: ${ten_sv}` );
-//     console.log( `sdt: ${sdt}` );
+//cập nhật dữ liệu
+function updateStudent( index,student_code,surname ,email,phone_number){
+    list[index][0] = student_code;
+    list[index ][1] = surname;
+    list[index ][2] = email;
+    list[index ][3] = phone_number;
+ 
+    document.getElementById('student_code').value       = '';
+    document.getElementById('surname').value            = '';
+    document.getElementById('email').value              = '';
+    document.getElementById('phone_number').value       = '';
 
-//     //cập nhật lại mảng danh_sach;
-
-//     /*
-//     danh_sach = [ 
-//         ['1','Nguyễn Văn A','0123456789'], // 0
-//         // 0      1             2
-//         ['2','Nguyễn Văn B','0943223222']  // 1
-//         //0       1             2 
-//     ];     
-//     */
-//     //chi_so = 1;
-//     danh_sach[index ][0] = ma_sv;
-//     danh_sach[index ][1] = ten_sv;
-//     danh_sach[index ][2] = sdt;
-
-//     //làm rỗng các input trong form
-//     document.getElementById('ma_sv').value          = '';
-//     document.getElementById('ho_ten').value         = '';
-//     document.getElementById('so_dien_thoai').value  = '';
-
-//      //cho nút đăng ký hiện lên, và ẩn nút cập nhật đi
-//     /*
-//         nut_cap_nhat, nut_dang_ky
-//     */
-//     let nut_cap_nhat = document.getElementById('nut_cap_nhat');
-//     let nut_dang_ky  = document.getElementById('nut_dang_ky');
+    let btn_update = document.getElementById('btn_update');
+    let btn_register  = document.getElementById('btn_register');
     
-//     nut_cap_nhat.style.display  = 'none';
-//     nut_dang_ky.style.display   = 'inline';
+    btn_update.style.display  = 'none';
+    btn_register.style.display   = 'inline';
 
-//     //gọi lại hàm hiển thị danh sách
-//     hien_thi_danh_sach();   
+    displaytbodyList();   
+}
 
-// }
+//xóa sinh vien
+function Delete(index) {
 
-// //xóa sinh vien
-// function xoa_sinh_vien(index) {
-//     /* 
-//         danh_sach = [ 
-//             ['1','Nguyễn Văn A','0123456789'],//0 của mảng danh_sach
-//             //0        1              2   
-//             ['2','Nguyễn Văn B','0943223222'],//1 của mảng danh_sach
-//         ];     
-//     */
-//     let xac_nhan = confirm('Bạn có chắc chắn xóa hay không ?');
-//     if( xac_nhan == true ){
-//         //người dùng đã nhấn vào OK
-//         //xóa phần tử trong mảng dựa vào chỉ số
-//         danh_sach.splice(index ,1);
+    let cfMsg = confirm('Delete information of Student ?');
+    if( cfMsg == true ){
+        //người dùng đã nhấn vào OK
+        //xóa phần tử trong mảng dựa vào chỉ số
+        list.splice(index ,1);
 
-//         //gọi lại hàm hiển thị danh sách
-//         hien_thi_danh_sach();
-//     }
-// }
+        //gọi lại hàm hiển thị danh sách
+        displaytbodyList();
+    }
+}
